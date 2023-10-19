bb69348e9baa95d294d8ec3480280ff4512fd012
e0k0308 <eric.keng@samsclub.com>
2023-10-19 10:12:12 -0500

    Header.js component done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from "react";
 import "./App.css";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import NucampLogo from "./app/assets/img/logo.png";
-import CampsiteCard from "./features/campsites/CampsiteCard";
-import { CAMPSITES } from "./app/shared/CAMPSITES";
 import CampsitesList from "./features/campsites/CampsitesList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <img src={NucampLogo} alt="nucamp logo" />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
+      <Header></Header>
       <CampsitesList />
     </div>
   );
