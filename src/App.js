3d9016510c1c6441efff0c458fee33214dbbca37
e0k0308 <eric.keng@samsclub.com>
2023-10-19 10:18:51 -0500

    Footer.js component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CampsitesList from "./features/campsites/CampsitesList";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
