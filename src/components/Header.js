e172bf8d5d4a5616d3ea8c8fcfdeb613d4c22adf
e0k0308 <eric.keng@samsclub.com>
2023-10-22 15:12:50 -0500

    header menu links fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,12 @@ import {
   NavbarToggler,
   Nav,
   NavItem,
-  Col,
 } from "reactstrap";
-import NavLink from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import NucampLogo from "../app/assets/img/logo.png";
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Navbar dark color="primary" sticky="top" expand="md">
       <NavbarBrand className="ms-5" href="/">
@@ -22,17 +21,25 @@ const Header = () => {
       <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
       <Collapse navbar isOpen={menuOpen}>
         <Nav className="ms-auto" navbar>
-          <NavItem className="nav-link" to="/">
-            <i className="fa fa-home fa-lg" /> Home
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              <i className="fa fa-home fa-lg" /> Home
+            </NavLink>
           </NavItem>
-          <NavItem className="nav-link" to="/directory">
-            <i className="fa fa-list fa-lg" /> Directory
+          <NavItem>
+            <NavLink className="nav-link" to="/directory">
+              <i className="fa fa-list fa-lg" /> Directory
+            </NavLink>
           </NavItem>
-          <NavItem className="nav-link" to="/about">
-            <i className="fa fa-info fa-lg" /> About
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              <i className="fa fa-info fa-lg" /> About
+            </NavLink>
           </NavItem>
-          <NavItem className="nav-link" to="/contact">
-            <i className="fa fa-address-card fa-lg" /> Contact
+          <NavItem>
+            <NavLink className="nav-link" to="/contact">
+              <i className="fa fa-address-card fa-lg" /> Contact
+            </NavLink>
           </NavItem>
         </Nav>
       </Collapse>
