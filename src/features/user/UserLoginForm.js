6c45b676f3434891058d74695473fa6f611e9a03
e0k0308 <eric.keng@samsclub.com>
2023-11-02 11:34:28 -0500

    userSlice and user state management done UserLoginForm created Added login button to Header and created login modal with logic done WIP - login validation logic

---
+++ src/features/user/UserLoginForm.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser, setCurrentUser } from "./userSlice";
+import defaultAvatar from "../../app/assets/img/unicorn.png";
+import {
+  Button,
+  FormGroup,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+} from "reactstrap";
+import { Field, Form, Formik } from "formik";
+
+const UserLoginForm = () => {
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+
+  const handleLogin = (values) => {
+    const loginUser = {
+      id: Date.now(),
+      avatar: defaultAvatar,
+      username: values.username,
+      password: values.password,
+    };
+    console.log("login values:", values);
+    dispatch(setCurrentUser(loginUser));
+    setLoginModalOpen(false);
+  };
+
+  return (
+    <>
+      <span className="navbar-text ml-auto">
+        {currentUser ? (
+          <div style={{ width: "4rem", height: "4rem" }}>
+            <img
+              src={defaultAvatar}
+              alt="avatar"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        ) : (
+          <Button
+            outline
+            onClick={() => setLoginModalOpen(true)}
+            style={{ color: "white", border: "1px solid white" }}
+          >
+            <i className="fa fa-sign-in fa-lg" /> Login
+          </Button>
+        )}
+      </span>
+      <Modal isOpen={loginModalOpen}>
+        <ModalHeader toggle={() => setLoginModalOpen(false)}>Login</ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{
+              username: "",
+              password: "",
+            }}
+            onSubmit={handleLogin}
+          >
+            <Form>
+              <FormGroup>
+                <Label id="username" htmlFor="username">
+                  Username{" "}
+                </Label>
+                <Field
+                  id="username"
+                  type="username"
+                  name="username"
+                  placeholder="Username"
+                  className="form-control"
+                ></Field>
+              </FormGroup>
+              <FormGroup>
+                <Label id="password" htmlFor="password">
+                  Password
+                </Label>
+                <Field
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="form-control"
+                ></Field>
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
+
+export default UserLoginForm;
